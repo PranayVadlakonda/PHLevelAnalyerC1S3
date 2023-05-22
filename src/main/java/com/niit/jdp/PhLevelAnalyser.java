05d531334c566486361f817f84fabb5d7d7d1c2f
pranay <pranayanand61@gmail.com>
2023-05-22 14:57:03 +0530

    else if ph level >= 7 and < 8 print else if construct

--- src/main/java/com/niit/jdp/PhLevelAnalyser.java
+++ src/main/java/com/niit/jdp/PhLevelAnalyser.java
@@ -19,8 +19,15 @@ public class PhLevelAnalyser {
         String statusOfAquarium="";
             if (phlevel < 7) {
                 statusOfAquarium = "pH value is low, partial water change is required";
-                
+            } else if (phLevel >= 7 && phLevel <= 8) {
+                statusOfAquarium = "pH value is fine";
+            } else if (phLevel > 8) {
+                statusOfAquarium = "pH value is high, partial water change is required";
             }
+            return statusOfAquarium;
+
+            }
+    }
 
 
 
