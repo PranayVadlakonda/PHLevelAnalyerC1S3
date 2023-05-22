8eb8434827fffae03b0ccd3cf2e063887012383c
pranay <pranayanand61@gmail.com>
2023-05-22 14:38:24 +0530

    initialized nre scanner and printed enter phlevel

---
+++ src/main/java/com/niit/jdp/PhLevelAnalyser.java
@@ -0,0 +1,14 @@
+package com.niit.jdp;
+
+import java.util.Scanner;
+
+public class PhLevelAnalyser {
+    public static void main(String[] args) {
+        String statusOfAquarium = "";
+        Scanner Scanner = new Scanner(system.in);
+        System.out.println("Enter phlevel :");
+        int phLevel = scanner.nextInt();
+        phLevelAnalyser phLevelAnalyser = new phLevelAnalyser;
+
+    }
+}
